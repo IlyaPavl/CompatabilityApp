132c3a15286c7807f65f3b946fa985f94a8f644b
IlyaPavl <ilyapav@icloud.com>
2023-07-19 23:38:02 +0300

    Added check for only-text input^ fixed progressBar and return to Initial Screen

--- CompatabilityApp/InitialViewController.swift
+++ CompatabilityApp/InitialViewController.swift
@@ -14,8 +14,10 @@ class InitialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemGray6
+        yourNameTF.returnKeyType = .next
+        partnerNameTF.returnKeyType = .done
         
-
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -26,10 +28,69 @@ class InitialViewController: UIViewController {
     }
     
     @IBAction func pressedButton() {
+        guard let firstName = yourNameTF.text, let secondName = partnerNameTF.text else {return}
+        
+        if firstName.isEmpty || secondName.isEmpty {
+            showAlert(title: "Ошибка!", message: "Введите оба имени")
+            return
+        }
         
         performSegue(withIdentifier: "resultSegue", sender: nil)
     }
     
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        yourNameTF.text = ""
+        partnerNameTF.text = ""
+    }
+    
+}
 
+extension InitialViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// напишем протокол, который поможет настроить отображение клавиатуры на экране
+extension InitialViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            pressedButton()
+        }
+        return true
+    }
+    
+    /*
+     ограничение для ввода только символов
+     1. Создается переменная allowedChar, которая представляет собой объединение символов из CharacterSet.letters (буквы) и CharacterSet.whitespaces (пробелы). Этот набор символов определяет, какие символы разрешены для ввода.
+     2. Создается переменная replacementCharSet, которая представляет собой CharacterSet, созданный из строки string. Этот набор символов представляет символы, которые будут вставлены или заменят указанный диапазон в текстовом поле.
+     3. Выполняется проверка с помощью метода isSubset(of:), чтобы определить, является ли набор символов replacementCharSet подмножеством набора разрешенных символов allowedChar.
+     4. Возвращается булево значение: true, если набор символов replacementCharSet является подмножеством allowedChar (т.е. символы допустимы для ввода); false, если есть хотя бы один символ в replacementCharSet, который не является разрешенным символом.
+     */
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedChar = CharacterSet.letters.union(.mySet)
+        let replacementCharSet = CharacterSet(charactersIn: string)
+        
+        return replacementCharSet.isSubset(of: allowedChar)
+    }
+}
+// делаем расширение для имен, которые пишутся через тире
+extension CharacterSet {
+    static var mySet: CharacterSet {
+        var symbols = CharacterSet()
+        symbols.insert(charactersIn: "-")
+        return symbols
+    }
 }
 
--- CompatabilityApp/ResultViewController.swift
+++ CompatabilityApp/ResultViewController.swift
@@ -9,16 +9,78 @@ import UIKit
 
 class ResultViewController: UIViewController {
     @IBOutlet weak var namesLabel: UILabel!
-    @IBOutlet weak var percentLevel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
     var firstName: String!
     var secondName: String!
+    
+    private var resultValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        resultValue = findResult()
+        
         namesLabel.text = "\(firstName ?? "") и \(secondName ?? "")"
+        
+        percentLabel.text = resultValue.formatted(.percent)
+        
+        // степень заполнения прогресс бара в зависимости от результата
+        progressBar.progress = Float(resultValue) / 100
+        
+        // настраиваем вид прогресс бара
+        progressBar.transform = .init(scaleX: 1, y: 2)
+    }
+    
+    private func findValue(for name: String) -> Int {
+        var count = 0
+        
+        for character in name.lowercased () {
+            switch character {
+            case "а", "и", "й", "ы", "у":
+                count += 1
+            case "б", "к", "р", "ш":
+                count += 2
+            case "с", "г", "л", "ц":
+                count += 3
+            case "д", "м", "т", "э":
+                count += 4
+            case "е", "х", "н", "ё":
+                count += 5
+            case "ю", "в", "ж", "я":
+                count += 6
+            case "о", "з", "ь", "ъ":
+                count += 7
+            case "ф", "п", "щ", "ч":
+                count += 8
+            default:
+                count += 0
+            }
+        }
+        return count
+    }
+    
+    private func findResult() -> Int {
+        var result = 0
+        
+        let firstValue = findValue(for: firstName)
+        let secondValue = findValue(for: secondName)
+        let absDifference = abs(firstValue - secondValue)
+        
+        switch absDifference {
+        case 0, 1, 2:
+            result = 100
+        case 3, 4:
+            result = 65
+        case 5:
+            result = 50
+        case 6:
+            result = 40
+        default:
+            result = 30
+        }
+        return result
     }
     
 }
